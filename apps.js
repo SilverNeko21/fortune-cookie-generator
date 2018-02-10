88ad9263f3a326b39aa088c18062cad4667e003c
SilverNeko21 <Larong2122@gmail.com>
2018-02-09 14:17:42 -1000

    dying

---
+++ apps.js
@@ -0,0 +1,6 @@
+var fortunesList = ["U gonna deaded", "Dirty Bob will visit u soon", "U will have to use da bathroom soon", "U will continue to generate fortunes", "U will actually believe that these are real", "U will fiddle the riddle"]
+
+var fortune ; 
+function generateFortuneCookie(arr){
+  document.getElementById("fortune-cookie-text").innerHTML = 
+}
