c20831b7642e19c8d355b7057ba58d5f21e97fbd
SilverNeko21 <larong2122@gmail.comgit config --global user.email larong2122@gmail.comgit config --global user.email larong2122@gmail.com>
2018-02-12 21:14:08 -1000

    I'M ON THE PATH TO FREEDOMgit add .!

--- apps.js
+++ apps.js
@@ -1,14 +1,16 @@
-var fortunesList = ["U gonna deaded", "Dirty Bob will visit u soon", "U will have to use da bathroom soon", "U will continue to generate fortunes", "U will actually believe that these are real", "U will fiddle the riddle"]
+var fortunesList = ["U gonna deaded!", "Dirty Bob will visit u soon.", "U will have to use da bathroom soon.", "U will continue to generate fortunes.", "U will actually believe that these are real.", "U will fiddle the riddle!"]
 
 var fortune ;
 
-
 function generateFortuneCookie() {
-	i = Math.floor((Math.random() * 6) + 0);
+	i = Math.floor((Math.random() * 6));
  	document.getElementById("fortune-cookie-text").innerHTML = fortunesList[i];
+ 	
  	var List = document.createElement("LI");
  	var Listing = document.createTextNode(fortunesList[i]);
  	List.appendChild(Listing);
  	document.getElementById("previous-fortunes").appendChild(List);
+ 	var before = document.getElementById("previous-fortunes");
+ 	before.insertBefore(List, before.childNodes[0]);
 }	
 	
