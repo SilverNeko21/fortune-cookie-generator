1e34b0442b153c3ca17735c82a72e7509a793dbe
SilverNeko21 <larong2122@gmail.comgit config --global user.email larong2122@gmail.comgit config --global user.email larong2122@gmail.com>
2018-02-12 15:49:04 -1000

    ON THE RIGHT WAY!

--- apps.js
+++ apps.js
@@ -1,6 +1,8 @@
 var fortunesList = ["U gonna deaded", "Dirty Bob will visit u soon", "U will have to use da bathroom soon", "U will continue to generate fortunes", "U will actually believe that these are real", "U will fiddle the riddle"]
 
-var fortune ; 
-function generateFortuneCookie(arr){
-  document.getElementById("fortune-cookie-text").innerHTML = 
+var fortune ;
+
+function generateFortuneCookie() {
+ 	document.getElementById("fortune-cookie-text").innerHTML = Math.floor((Math.random() * 7) + 0);
 }
+	
