f92ea58860e4f8b605b9924b2c9af312b60a2b41
SilverNeko21 <larong2122@gmail.comgit config --global user.email larong2122@gmail.comgit config --global user.email larong2122@gmail.com>
2018-02-14 17:54:20 -1000

    WOW

--- apps.js
+++ apps.js
@@ -2,6 +2,12 @@ var fortunesList = ["You're gonna die!", "Dirty Bob will visit you soon.", "You 
 
 var fortune ;
 
+function aloha() {
+	if (fortunesList.length == 0){
+		fortunesList.splice(0,0,"You're gonna die!", "Dirty Bob will visit you soon.", "You will have to use the bathroom soon.", "You will continue to generate fortunes.", "You will actually believe that these are real.", "You will fiddle the riddle!", "Burger King Foot Lettuce?", "You know I had to do it to em.")
+	}
+}
+
 function generateFortuneCookie() {
   if (fortunesList.length > 0) {
     i = Math.floor((Math.random() * fortunesList.length));
@@ -14,8 +20,6 @@ function generateFortuneCookie() {
  	  var before = document.getElementById("previous-fortunes");
  	  before.insertBefore(List, before.childNodes[0]);
  	  fortunesList.splice(i , 1);
-  }else if (fortunesList.length == 0){
-    fortunesList.splice(0,0,"You're gonna die!", "Dirty Bob will visit you soon.", "You will have to use the bathroom soon.", "You will continue to generate fortunes.", "You will actually believe that these are real.", "You will fiddle the riddle!", "Burger King Foot Lettuce?", "You know I had to do it to em.")
   }
 }	
 
