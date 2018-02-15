43fac809813d1cf26012041e738f0ae3b5f96c25
SilverNeko21 <Larong2122@gmail.com>
2018-02-14 14:41:20 -1000

    WORKING ON IT

--- apps.js
+++ apps.js
@@ -3,15 +3,19 @@ var fortunesList = ["You're gonna die!", "Dirty Bob will visit you soon.", "You 
 var fortune ;
 
 function generateFortuneCookie() {
-	i = Math.floor((Math.random() * 8));
- 	document.getElementById("fortune-cookie-text").innerHTML = fortunesList[i];
+  if (fortunesList.length > 0) {
+    i = Math.floor((Math.random() * fortunesList.length));
+ 	  document.getElementById("fortune-cookie-text").innerHTML = fortunesList[i];
  	
- 	var List = document.createElement("LI");
- 	var Listing = document.createTextNode(fortunesList[i]);
- 	List.appendChild(Listing);
- 	document.getElementById("previous-fortunes").appendChild(List);
- 	var before = document.getElementById("previous-fortunes");
- 	before.insertBefore(List, before.childNodes[0]);
- 	fortunesList.splice(fortunesList[1] , fortunesList[i]);	
+ 	  var List = document.createElement("LI");
+ 	  var Listing = document.createTextNode(fortunesList[i]);
+ 	  List.appendChild(Listing);
+ 	  document.getElementById("previous-fortunes").appendChild(List);
+ 	  var before = document.getElementById("previous-fortunes");
+ 	  before.insertBefore(List, before.childNodes[0]);
+ 	  fortunesList.splice(i , 1);
+  }else if (fortunesList.length == 0){
+    fortunesList.splice(0,0,"You're gonna die!", "Dirty Bob will visit you soon.", "You will have to use the bathroom soon.", "You will continue to generate fortunes.", "You will actually believe that these are real.", "You will fiddle the riddle!", "Burger King Foot Lettuce?", "You know I had to do it to em.")
+  }
 }	
-	
+
