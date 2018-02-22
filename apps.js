0da2e2fde61d77ca02e20f7a934f61c3c17bccb5
SilverNeko21 <larong2122@gmail.comgit config --global user.email larong2122@gmail.comgit config --global user.email larong2122@gmail.com>
2018-02-21 18:53:09 -1000

    removing unneeded things

--- apps.js
+++ apps.js
@@ -1,6 +1,5 @@
 var fortunesList = ["You're gonna die!", "Dirty Bob will visit you soon.", "You will have to use the bathroom soon.", "You will continue to generate fortunes.", "You will actually believe that these are real.", "You will fiddle the riddle!", "Burger King Foot Lettuce?", "You know I had to do it to em.", "You will visit jones barbecue and foot massage.","You will figure out the solution soon.", "Bob Ross will retuen.", "You will find a four leaf clover.", "Helter Skelter will rise again.", "You will find the secret dolphin hide out.", "The secret ingredient to my secret ingredient soup is your left toe.", "You will have a happy life.", "Donald Trump will be president for 8 years."];
 
-var before = document.getElementById("previous-fortunes");
 
 function generateFortuneCookie() {
   if (fortunesList.length > 0) {
